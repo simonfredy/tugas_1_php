54b7e0ed244740f09f172f0fb371b0f92ceab3da
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-04-11 10:50:45 +0700

    Tugas 1 PHP

---
+++ tugas1.php
@@ -0,0 +1,23 @@
+<?php
+    $ag = 13000;
+    $ab = 15000;
+    $as = 13000;
+    $tg = 1000;
+    $thg = 1000;
+    $st = 3000;
+    $np = 5000;
+    $et = 4000;
+    $etb = 5000;
+
+    echo "<h1>Daftar Menu</h1>";
+    echo "<li>Ayam Goreng = ". " Rp. " . $ag;
+    echo "<li>Ayam Bakar = ". " Rp. " . $ab;
+    echo "<li>Ayam Sayur = ". " Rp. " . $as;
+    echo "<li>Tempe Goreng = ". " Rp. " . $tg;
+    echo "<li>Tahu Goreng = ". " Rp. " . $thg;
+    echo "<li>Soto = ". " Rp. " . $st;
+    echo "<li>Nasi Putih = ". " Rp. " . $np;
+    echo "<li>Es Teh = ". " Rp. " . $et;
+    echo "<li>Es Tebu = ". " Rp ". $etb;
+
+?>
---
+++ tugas1_2.php
@@ -0,0 +1,51 @@
+<?php
+    $cireng = 1000;
+    $esduren = 15000;
+    $esmilo = 20000;
+    $esteh = 5000;
+    $piscok = 1000;
+
+    echo "<h1>Daftar Menu</h1>";
+    
+    // Tabel
+
+    echo "<table border='1'>";
+
+    echo "<tr>";
+    echo "<td>No.</td>";
+    echo "<td>Nama Menu</td>";
+    echo "<td>Harga</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>1</td>";
+    echo "<td>Cireng</td>";
+    echo "<td>Rp. " ."$cireng". "</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>2</td>";
+    echo "<td>Es Duren</td>";
+    echo "<td>Rp. " ."$esduren". "</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>3</td>";
+    echo "<td>Es Milo</td>";
+    echo "<td>Rp. " ."$esmilo". "</td>";;
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>4</td>";
+    echo "<td>Es Teh</td>";
+    echo "<td>Rp. " ."$esteh". "</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td>5</td>";
+    echo "<td>Piscok</td>";
+    echo "<td>Rp. " ."$piscok". "</td>";;
+    echo "</tr>";
+
+    echo "</table>";
+?>
